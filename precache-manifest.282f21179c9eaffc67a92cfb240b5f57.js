01b797be1e1916a32336f26f25f3ee24dcc13143
Eugene <eugene97@connect.hku.hk>
2019-08-30 17:36:30 +0800

    Updates

---
+++ precache-manifest.282f21179c9eaffc67a92cfb240b5f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d39c301bca478a7dd39d1e47df93d0a",
+    "url": "/yujin97/robofriends/index.html"
+  },
+  {
+    "revision": "d9db4002ec5fbeff089d",
+    "url": "/yujin97/robofriends/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "74b96858e072e0900286",
+    "url": "/yujin97/robofriends/static/js/2.24ef4cb2.chunk.js"
+  },
+  {
+    "revision": "d9db4002ec5fbeff089d",
+    "url": "/yujin97/robofriends/static/js/main.5936ec4d.chunk.js"
+  },
+  {
+    "revision": "0947e8962e27af3c4ad1",
+    "url": "/yujin97/robofriends/static/js/runtime~main.b201c421.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yujin97/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
