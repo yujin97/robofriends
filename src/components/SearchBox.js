ed40e4609771a30f3ee643262d8d1ccbe01fcc55
Eugene <eugene97@connect.hku.hk>
2019-09-04 21:09:13 +0800

    first push

---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = ( { searchChange } ) => {
+	return (
+		<div className ='pa2'>
+		<input 
+		className = 'pa3 ba b--green bg-lightest-blue'
+		type = 'search' 
+		onChange = {searchChange}
+		placeholder='search robots'/>
+		</div>
+		)
+}
+
+export default SearchBox;
