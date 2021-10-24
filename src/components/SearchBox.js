de0d52ea0ff5f0d06e93470a51c1eddc1a9800c7
Eugene <eugene97@connect.hku.hk>
2021-10-24 23:13:11 +0800

    pwa version

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const SearchBox = ( { searchChange } ) => {
-	return (
-		<div className ='pa2'>
-		<input 
-		className = 'pa3 ba b--green bg-lightest-blue'
-		type = 'search' 
-		onChange = {searchChange}
-		placeholder='search robots'/>
-		</div>
-		)
-}
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        aria-label="Search Robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        onChange={searchChange}
+        placeholder="search robots"
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
